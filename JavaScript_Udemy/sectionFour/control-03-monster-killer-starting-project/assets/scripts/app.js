873237a61d0efdbf7d0d07622b7bd544882660fd
Mojo <12233836+nullBadgers@users.noreply.github.com>
2021-06-23 19:05:18 +0100

    updated attackMonster

--- JavaScript_Udemy/sectionFour/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ JavaScript_Udemy/sectionFour/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,5 +1,7 @@
 // written in capital to show it's a GLOBAL value
-const ATTACK_VALUE = 10;
+// these values do not change!
+const ATTACK_VALUE = 8;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -8,15 +10,32 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(attackMode) {
+  let maxDamage;
+  if (attackMode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else if (attackMode === "STRONG_ATTACK") {
+    maxDamage = STRONG_ATTACK_VALUE;
+  } 
+  const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  if (currentMonsterHealth <= 0) {
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert("You win!");
-  } else if (currentPlayerHealth <= 0) {
-    alert("You lost!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You lose!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("Draw!");
   }
 }
 
+function attackHandler() {
+  attackMonster("ATTACK");
+}
+
+function strongAttackHandler() {
+  attackMonster("STRONG_ATTACK");
+}
+
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
